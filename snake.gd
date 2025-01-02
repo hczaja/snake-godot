21b05dc3f3a099f2358d02372f94c8b3378cd8bb
hczaja <hubert_czaja@o2.pl>
2025-01-02 16:04:25 +0100

    add simple signal and handle collision with some body

--- snake.gd
+++ snake.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 128
 
@@ -6,10 +7,11 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	var velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_right"):
@@ -28,5 +30,13 @@ func _process(delta):
 		$Sprite2D.flip_h = false
 	if velocity.y != 0:
 		$Sprite2D.flip_v = velocity.y < 0
-	
-	pass
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
